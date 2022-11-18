8bccf5d829f24cefa8c6c91d2b25d12e0fa747dc
Arlo <arlowotherspoon@gmail.com>
2022-11-18 16:29:59 +1300

    Added script to import settings.

---
+++ run.ps1
@@ -0,0 +1,2 @@
+cd "$env:USERPROFILE\code\git\wt-profile"
+Copy-Item .\settings.json -Destination "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Force
