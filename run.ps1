a330f0491765ab4cad4c239cc289ee669adb8e67
Arlo Wotherspoon <metalgeisha@outlook.co.nz>
2023-08-05 22:10:25 +1200

    updated script

--- run.ps1
+++ run.ps1
@@ -1,2 +1,3 @@
 cd "$env:USERPROFILE\code\git\wt-profile"
-Copy-Item .\settings.json -Destination "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Force
+
+Copy-Item .\settings.json -Destination "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json" -Force
