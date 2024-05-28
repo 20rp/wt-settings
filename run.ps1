d29d12e3823d88d1ab3b76bef37284683c636c01
Arlo Wotherspoon <65660355+20rp@users.noreply.github.com>
2024-05-28 13:38:13 +1200

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,3 +1,3 @@
-cd "$env:USERPROFILE\code\git\wt-profile"
+cd ".\wt-profile"
 
 Copy-Item .\settings.json -Destination "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json" -Force
